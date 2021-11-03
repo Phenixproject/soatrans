987da595b2ce6eb0d4d5c3d8d67aa0ba5ab15a07
MiaroAnthony <miaroantoine12@gmail.com>
2021-11-03 16:15:53 +0300

    Premiere modification

--- models.py
+++ models.py
@@ -33,6 +33,8 @@ from .managers import *
 
 class Role(models.Model) :
     libelle_role = models.CharField(max_length = 100)
+    
+
 
 class Utilisateur(AbstractUser):
     username = models.CharField(max_length=100, unique=True, blank=False)
@@ -42,14 +44,19 @@ class Utilisateur(AbstractUser):
     CIN = models.CharField(max_length=12, null=True)
     sexe = models.BooleanField(default=False)
     date_inscription = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to= "images/clients")
     telephone_utilisateur = models.CharField(max_length = 10, blank = False)
     statut_utilisateur = models.BooleanField(default = False, blank = False) # Si l'utilisateur est déjà confirmé
 
     objects = UtilisateurManager()
     USERNAME_FIELD = 'username'
     
+    
+    
 class Local(models.Model):
     local = models.CharField(max_length = 50, blank = False)
+    
+    
     
 class Personnel(models.Model):
     username_personnel = models.CharField(max_length = 100, unique = True, blank = False)
@@ -60,42 +67,53 @@ class Personnel(models.Model):
     telephone_personnel = models.CharField(max_length = 10, blank = False)
     password_personnel = models.CharField(max_length = 50, blank = False)
     statut_personnel = models.BooleanField(blank = False, default = False)
-    local = models.ForeignKey(Local, related_name = "personnel_local", on_delete = models.CASCADE, null = true)
+    photo = models.ImageField(upload_to= "images/personnels")
+    local = models.ForeignKey(Local, related_name = "personnel_local", on_delete = models.CASCADE, null = True)
     role = models.ForeignKey(Role, related_name="personnel_role", on_delete=models.CASCADE, null= True)
 
     #def __str__(self):
         #return "{}{}".format(self.last_name)
+        
+        
 
 class Horaire(models.Model):
     heure = models.CharField(max_length=20, null=False)
+    
+    
 
 class ClasseVoiture(models.Model):
     classe = models.CharField(max_length=20, blank=False, null=False)
+    
+
 
 class Destination(models.Model):
     depart = models.CharField(max_length=100, blank=False)
     arrive = models.CharField(max_length=100, blank=False)
+    
+
 
 class HoraireClasse(models.Model):
     horaire = models.ForeignKey(Horaire, related_name="horaireclasse_horaire", on_delete=models.CASCADE)
     classe = models.ForeignKey(ClasseVoiture, related_name="horaireclasse_classevoiture", on_delete=models.CASCADE)
     destination = models.ForeignKey(Destination, related_name="horaireclasse_destination", on_delete=models.CASCADE)
-
+    
+   
 class Voiture(models.Model):
     numero_voiture = models.CharField(max_length = 7, blank = False)
     etat_voiture = models.BooleanField(default = True, blank = False)
-    classe = models.ForeignKey(ClasseVoiture, related_name="voiture_classevoiture", on_delete=models.CASCADE)
-
-
+    horaire = models.ForeignKey(HoraireClasse, related_name="voiture_classevoiture", on_delete=models.CASCADE)
+   
+ 
+    
 class Reservation(models.Model):
     montant_paye = models.BigIntegerField(blank = False)
-    avance_paye = models.BigIntegerField(defaul)
+    avance_paye = models.BigIntegerField(default=0)
     position_place = models.CharField(max_length = 100, blank = False)
     #modification... Nasiana date ilay izy mba ahafahana mi filtrer an'ny olona nanao reservaiton tamina date iray
     date = models.DateTimeField(auto_now_add = True)
     utilisateur = models.ForeignKey(Utilisateur, related_name="reservation_utilisateur", on_delete=models.CASCADE)
     voiture = models.ForeignKey(Voiture, related_name="reservation_voiture", on_delete=models.CASCADE)
-    horaireclasse = models.ForeignKey(HoraireClasse, related_name="reservation_horaireclasse", on_delete=models.CASCADE)
+    horaireclasse = models.ForeignKey(HoraireClasse, related_name="reservation_horaireclasse", on_delete=models.CASCADE) 
 
 
 
@@ -103,5 +121,5 @@ class Paiement(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     montant = models.IntegerField()
     personnel = models.ForeignKey(Personnel, related_name="montant_personnel", on_delete=models.CASCADE)
-    reservaiton = models.ForeignKey(Reservation, related_name="reservations_paiement", on_delete=models.CASCADE)
+    reservation = models.ForeignKey(Reservation, related_name="reservations_paiement", on_delete=models.CASCADE)
 
